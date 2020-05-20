1b3339513f1aaebc0c8ca7dd79263709a8355522
James <james.jiang13@gmail.com>
2020-05-20 11:34:44 -0700

    Fixed map to only Latin America

--- public/javascripts/map/map.js
+++ public/javascripts/map/map.js
@@ -3,13 +3,25 @@ const svg = d3.select('svg');
 const projection = d3.geoMercator().scale(150);
 const pathGenerator = d3.geoPath().projection(projection);
 
-d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+d3.json('/javascripts/map/latinamerica.json')
   .then((topo) => {
-    const countries = topojson.feature(topo, topo.objects.countries);
-    console.log(countries);
+    console.log(topo.features);
     svg.selectAll('path')
-      .data(countries.features)
+      .data(topo.features)
       .enter()
       .append('path')
       .attr('d', pathGenerator);
   });
+
+// d3.json('https://unpkg.com/world-atlas@1.1.4/world/50m.json')
+//   .then((topo) => {
+//     // console.log(topo)
+//     const countries = topojson.feature(topo, topo.objects.countries);
+//     // console.log(countries);
+//     console.log(countries.features)
+//     svg.selectAll('path')
+//       .data(countries.features)
+//       .enter()
+//       .append('path')
+//       .attr('d', pathGenerator);
+//   });
